d184eb57ee78ea27a2cacc555c874e36abedcd29
ezaddeen <ezaddeenaltahesh@gmail.com>
2026-06-09 14:29:56 +0300

    docker

--- land-classification-frontend/next.config.ts
+++ land-classification-frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+  },
 };
 
 export default nextConfig;
